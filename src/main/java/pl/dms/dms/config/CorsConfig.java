78a60b80f617762f32bf73757ea1dd4e3d2486cd
JBryskiewicz <kuba@fiku.eu>
2024-02-06 11:34:32 +0100

    cors include local dev

--- src/main/java/pl/dms/dms/config/CorsConfig.java
+++ src/main/java/pl/dms/dms/config/CorsConfig.java
@@ -12,7 +12,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://tt-session-manager.vercel.app")
+                .allowedOrigins(
+                        "https://tt-session-manager.vercel.app",
+                        "http://localhost:5173"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
